82136df4342147020b44a7b721d589dd5cf1ba88
Simone Arcari <simonearcari@outlook.it>
2023-04-16 12:14:13 +0200

    df

--- workspace1/GtkWindow-example/GtkWindow-example.c
+++ workspace1/GtkWindow-example/GtkWindow-example.c
@@ -5,15 +5,15 @@
 #define WINDOW_HEIGHT 520
 #define CONTAINER_SPACING 0
 #define ICON_NAME   "nome icona"
-#define BUTTON_LABEL    "Fullscreen"
+#define BUTTON_LABEL_FC    "Fullscreen"
+#define BUTTON_LABEL_UFC    "Unfullscreen"
 
 
 static void activate(GtkApplication *app, gpointer user_data);
 static void fullscreen_switch(GtkWidget *widget, gpointer user_data);
 
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     GtkApplication *app;
     int status;
 
@@ -26,14 +26,13 @@ int main(int argc, char **argv)
 }
 
 
-static void activate(GtkApplication *app, gpointer user_data)
-{
+static void activate(GtkApplication *app, gpointer user_data) {
     GtkWidget *window;
     GtkWidget *container;
     GtkWidget *button;
     const char *str;
-    gboolean fullscreen_flag = false;
-    gpointer data_pointer = &fullscreen_flag;
+    //gboolean fullscreen_flag = false;
+    //gpointer data_pointer = &fullscreen_flag;
 
     /* window */
     window = gtk_application_window_new(app);
@@ -55,16 +54,29 @@ static void activate(GtkApplication *app, gpointer user_data)
     gtk_widget_set_valign(container, GTK_ALIGN_CENTER);
 
     /* button */
-    button = gtk_button_new_with_label(BUTTON_LABEL);
-    g_signal_connect(button, "clicked", G_CALLBACK(fullscreen_switch), data_pointer);
+    button = gtk_button_new_with_label(BUTTON_LABEL_FC);
+    g_signal_connect(button, "clicked", G_CALLBACK(fullscreen_switch), window);
+
+    /* appending */
+    gtk_window_set_child(GTK_WINDOW(window), container);
+    gtk_box_append(GTK_BOX(container), button);
 
     /* set visible */
 	gtk_widget_set_visible(window, true);
 
-    gtk_window_fullscreen(GTK_WINDOW(window));
+    //gtk_window_fullscreen(GTK_WINDOW(window));
 }
 
 
-static void fullscreen_switch(GtkWidget *widget, gpointer user_data) {
-    //finire...
+static void fullscreen_switch(GtkWidget *button, gpointer window) {
+
+    if(gtk_window_is_fullscreen(GTK_WINDOW(window))) {
+
+        gtk_window_unfullscreen(GTK_WINDOW(window));
+        gtk_button_set_label(GTK_BUTTON(button), BUTTON_LABEL_FC);
+    }else {
+
+        gtk_window_fullscreen(GTK_WINDOW(window));
+        gtk_button_set_label(GTK_BUTTON(button), BUTTON_LABEL_UFC);
+    }
 }   
